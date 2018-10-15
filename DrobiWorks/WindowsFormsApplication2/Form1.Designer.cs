d12cf3a0f38ae26425fc0975780ab99312ab2898
dannydaniels <d.daniels@mail.ru>
2018-10-15 09:13:56 +0300

    Upload project dir

---
+++ DrobiWorks/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button12 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(47, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(47, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(173, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Числитель";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(173, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Знаменатель";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(12, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(247, 123);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Ответ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(265, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 20);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Сложение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(265, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 20);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Вычитание";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(265, 62);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(158, 20);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Умножение";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(265, 88);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(158, 20);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Деление";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(265, 114);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(158, 20);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Сокращение";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(265, 140);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(74, 20);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Степень 1";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(265, 166);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(158, 20);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "==";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(265, 192);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(158, 20);
+            this.button8.TabIndex = 13;
+            this.button8.Text = ">";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(265, 218);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(158, 20);
+            this.button9.TabIndex = 14;
+            this.button9.Text = "<";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(265, 244);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(158, 20);
+            this.button10.TabIndex = 15;
+            this.button10.Text = ">=";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(265, 270);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(158, 20);
+            this.button11.TabIndex = 16;
+            this.button11.Text = "<=";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 111);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(47, 20);
+            this.textBox3.TabIndex = 17;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(55, 92);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Степень";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(12, 29);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "----------";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(107, 11);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(47, 20);
+            this.textBox4.TabIndex = 20;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(107, 46);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(47, 20);
+            this.textBox5.TabIndex = 21;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(107, 30);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 13);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "----------";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(107, 111);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(47, 20);
+            this.textBox6.TabIndex = 23;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(349, 140);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(74, 20);
+            this.button12.TabIndex = 24;
+            this.button12.Text = "Степень 2";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 298);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button12;
+    }
+}
+
---
+++ DrobiWorks/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public int A1;
+        public float B1;
+        public int A2;
+        public float B2;
+        public int n;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) // +
+        {
+            Fraction f1 = new Fraction(A1 = Convert.ToInt32(textBox1.Text), B1 = Convert.ToSingle(textBox2.Text));
+            Fraction f2 = new Fraction(A2 = Convert.ToInt32(textBox4.Text), B2 = Convert.ToSingle(textBox5.Text));
+            Fraction f3 = f1 + f2;
+            label3.Text = "Ответ при сложении: " + Convert.ToString(f3);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) // -
+        {
+            Fraction f1 = new Fraction(A1 = Convert.ToInt32(textBox1.Text), B1 = Convert.ToSingle(textBox2.Text));
+            Fraction f2 = new Fraction(A2 = Convert.ToInt32(textBox4.Text), B2 = Convert.ToSingle(textBox5.Text));
+            Fraction f3 = f1 - f2;
+            label3.Text = "Ответпри вычитании: " + Convert.ToString(f3);
+        }
+
+        private void button3_Click(object sender, EventArgs e) // *
+        {
+            Fraction f1 = new Fraction(A1 = Convert.ToInt32(textBox1.Text), B1 = Convert.ToSingle(textBox2.Text));
+            Fraction f2 = new Fraction(A2 = Convert.ToInt32(textBox4.Text), B2 = Convert.ToSingle(textBox5.Text));
+            Fraction f3 = f1 * f2;
+            label3.Text = "Ответ при умножении: " + Convert.ToString(f3);
+        }
+
+        private void button4_Click(object sender, EventArgs e) // /
+        {
+            Fraction f1 = new Fraction(A1 = Convert.ToInt32(textBox1.Text), B1 = Convert.ToSingle(textBox2.Text));
+            Fraction f2 = new Fraction(A2 = Convert.ToInt32(textBox4.Text), B2 = Convert.ToSingle(textBox5.Text));
+            Fraction f3 = f1 / f2;
+            label3.Text = "Ответ при делении: " + Convert.ToString(f3);
+        }
+
+        private void button5_Click(object sender, EventArgs e) // Cut
+        {
+            Fraction f = new Fraction(A1 = Convert.ToInt32(textBox1.Text), B1 = Convert.ToSingle(textBox2.Text));
+            Fraction cuted = Fraction.Cut(f);
+            label3.Text = "Сокращение дроби: " + Convert.ToString(cuted);
+        }
+
+        private void button6_Click(object sender, EventArgs e) // Степень
+        {
+            Fraction f1 = new Fraction(A1 = Convert.ToInt32(textBox1.Text), B1 = Convert.ToSingle(textBox2.Text));
+            Fraction f2 = new Fraction(n = Convert.ToInt32(textBox3.Text), n = Convert.ToInt32(textBox3.Text));
+            Fraction f3 = Fraction.Pow(f1,f2);
+            label3.Text = "Дробь  " + f1 + "  в степени  " + n + "  =  " + Convert.ToString(f3);
+        }
+
+        private void button7_Click(object sender, EventArgs e) // Equal
+        {
+            Fraction f1 = new Fraction(A1 = Convert.ToInt32(textBox1.Text), B1 = Convert.ToSingle(textBox2.Text));
+            Fraction f2 = new Fraction(A2 = Convert.ToInt32(textBox4.Text), B2 = Convert.ToSingle(textBox5.Text));
+            bool result = Fraction.FractionEqual(f1, f2);
+            if (result == true)
+            {
+                label3.Text = "Дроби равны";
+            }
+            else
+            {
+                label3.Text = "Дроби не равны";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e) // >
+        {
+            Fraction f1 = new Fraction(A1 = Convert.ToInt32(textBox1.Text), B1 = Convert.ToSingle(textBox2.Text));
+            Fraction f2 = new Fraction(A2 = Convert.ToInt32(textBox4.Text), B2 = Convert.ToSingle(textBox5.Text));
+            bool result = Fraction.FractionBigger(f1, f2);
+            if (result == true)
+            {
+                label3.Text = "Дробь  " + f1 + "  больше  " + f2;
+            }
+            else
+            {
+                label3.Text = "Дробь  " + f2 + "  больше  " + f1;
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e) // <
+        {
+            Fraction f1 = new Fraction(A1 = Convert.ToInt32(textBox1.Text), B1 = Convert.ToSingle(textBox2.Text));
+            Fraction f2 = new Fraction(A2 = Convert.ToInt32(textBox4.Text), B2 = Convert.ToSingle(textBox5.Text));
+            bool result = Fraction.FractionSmaller(f1, f2);
+            if (result == true)
+            {
+                label3.Text = "Дробь  " + f1 + "  меньше  " + f2;
+            }
+            else
+            {
+                label3.Text = "Дробь  " + f2 + "  меньше  " + f1;
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e) // >=
+        {
+            Fraction f1 = new Fraction(A1 = Convert.ToInt32(textBox1.Text), B1 = Convert.ToSingle(textBox2.Text));
+            Fraction f2 = new Fraction(A2 = Convert.ToInt32(textBox4.Text), B2 = Convert.ToSingle(textBox5.Text));
+            bool result = Fraction.FractionBiggerOrEqual(f1, f2);
+            if (result == true)
+            {
+                label3.Text = "Дробь  " + f1 + "  больше либо равна  " + f2;
+            }
+            else
+            {
+                label3.Text = "Дробь  " + f2 + "  больше либо равна  " + f1;
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e) // <=
+        {
+            Fraction f1 = new Fraction(A1 = Convert.ToInt32(textBox1.Text), B1 = Convert.ToSingle(textBox2.Text));
+            Fraction f2 = new Fraction(A2 = Convert.ToInt32(textBox4.Text), B2 = Convert.ToSingle(textBox5.Text));
+            bool result = Fraction.FractionSmallerOrEqual(f1, f2);
+            if (result == true)
+            {
+                label3.Text = "Дробь  " + f1 + "  меньше либо равна  " + f2;
+            }
+            else
+            {
+                label3.Text = "Дробь  " + f2 + "  меньше либо равна  " + f1;
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            Fraction f1 = new Fraction(A1 = Convert.ToInt32(textBox4.Text), B1 = Convert.ToSingle(textBox5.Text));
+            Fraction f2 = new Fraction(n = Convert.ToInt32(textBox6.Text), n = Convert.ToInt32(textBox6.Text));
+            Fraction f3 = Fraction.Pow(f1, f2);
+            label3.Text = "Дробь  " + f1 + "  в степени  " + n + "  =  " + Convert.ToString(f3);
+        }
+    }
+
+    public class Fraction
+    {
+        private int p;
+        private float q;
+
+        public int P
+        {
+            get
+            {
+                return p;
+            }
+        }
+
+        public float Q
+        {
+            get
+            {
+                return q;
+            }
+        }
+
+        public Fraction(int givenP, float givenQ)
+        {
+            p = givenP;
+            q = givenQ;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}/{1}", p, q);
+        }
+
+        public static Fraction operator +(Fraction f1, Fraction f2)
+        {
+            float denominator = f1.Q + f2.Q;
+            int numerator = (int)(f1.P + f2.P);
+            return new Fraction(numerator, denominator);
+        }   // Оператор сложения
+
+        public static Fraction operator -(Fraction f1, Fraction f2)
+        {
+            float denominator = f1.Q + f2.Q;
+            int numerator = (int)(f1.P - f2.P);
+            return new Fraction(numerator, denominator);
+        }   // Оператор вычитания
+
+        public static Fraction operator *(Fraction f1, Fraction f2)
+        {
+            float denominator = f1.Q * f2.Q;
+            int numerator = (int)(f1.P * f2.P);
+            return new Fraction(numerator, denominator);
+        }   // Оператор умножения
+
+        public static Fraction operator /(Fraction f1, Fraction f2)
+        {
+            float denominator = f1.Q * f2.P;
+            int numerator = (int)(f1.P * f2.Q);
+            return new Fraction(numerator, denominator);
+        }   // Оператор деления
+
+        public static Fraction Cut(Fraction f)
+        {
+            int min = (int)Math.Min(f.P, f.Q);
+            bool cut = false;
+            for (int i = min; i >= 2; i--)
+            {
+                if (f.P % i == 0 && f.Q % i == 0)
+                {
+                    cut = true;
+                    break;
+                }
+            }
+            if (!cut)
+            {
+                return f;
+            }
+            else
+            {
+                return new Fraction(f.P / min, f.Q / min);
+            }
+        }   // Сокращение дроби.
+
+        public static bool FractionEqual(Fraction f1, Fraction f2)
+        {
+            if (f1.P == f2.P && f2.Q == f1.Q)
+            {
+                return true;
+            }
+            Decimal d1 = Convert.ToDecimal(f1.P) / Convert.ToDecimal(f1.Q);
+            Decimal d2 = Convert.ToDecimal(f2.P) / Convert.ToDecimal(f2.Q);
+            return d1 == d2;
+        }
+        // Эквивалент
+
+        public static bool FractionBigger(Fraction f1, Fraction f2)
+        {
+            Decimal d1 = Convert.ToDecimal(f1.P) / Convert.ToDecimal(f1.Q);
+            Decimal d2 = Convert.ToDecimal(f2.P) / Convert.ToDecimal(f2.Q);
+            return d1 == d2;
+
+        }
+        // >
+
+        public static bool FractionSmaller(Fraction f1, Fraction f2)
+        {
+            Decimal d1 = Convert.ToDecimal(f1.P) / Convert.ToDecimal(f1.Q);
+            Decimal d2 = Convert.ToDecimal(f2.P) / Convert.ToDecimal(f2.Q);
+            return d1 == d2;
+        }
+        // <
+
+        public static bool FractionBiggerOrEqual(Fraction f1, Fraction f2)
+        {
+            Decimal d1 = Convert.ToDecimal(f1.P) / Convert.ToDecimal(f1.Q);
+            Decimal d2 = Convert.ToDecimal(f2.P) / Convert.ToDecimal(f2.Q);
+            if (d1 > d2 || d1 == d2)
+            {
+                return true;
+            }
+            return false;
+        }
+        // >=
+
+        public static bool FractionSmallerOrEqual(Fraction f1, Fraction f2)
+        {
+            Decimal d1 = Convert.ToDecimal(f1.P) / Convert.ToDecimal(f1.Q);
+            Decimal d2 = Convert.ToDecimal(f2.P) / Convert.ToDecimal(f2.Q);
+            if (d1 < d2 || d1 == d2)
+            {
+                return true;
+            }
+            return false;
+        }
+        // <=
+        public static Fraction Pow (Fraction f1, Fraction f2)
+        {
+            double denominator = Math.Pow(f1.Q,f2.Q);
+            double numerator = Math.Pow(f1.P,f2.P);          
+            return new Fraction(Convert.ToInt32(numerator), Convert.ToSingle(denominator));
+        }
+    }
+
+}
